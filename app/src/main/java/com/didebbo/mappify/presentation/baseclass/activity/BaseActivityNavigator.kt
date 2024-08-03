0f7c8d9a501c29a2f268f4f60860d030ef9645a4
Gianluca Napoletano <gianluca@192.168.1.51>
2024-08-03 12:54:04 +0200

    handle bottomNavigationBar

--- app/src/main/java/com/didebbo/mappify/presentation/baseclass/activity/BaseActivityNavigator.kt
+++ app/src/main/java/com/didebbo/mappify/presentation/baseclass/activity/BaseActivityNavigator.kt
@@ -3,6 +3,9 @@ package com.didebbo.mappify.presentation.baseclass.activity
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
@@ -10,9 +13,12 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.didebbo.mappify.presentation.baseclass.fragment.page.BaseFragmentDestination
 import com.github.didebbo.mappify.R
 import com.github.didebbo.mappify.databinding.BaseActivityLayoutBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.lang.Exception
 
 abstract class BaseActivityNavigator<VM: ViewModel>(): AppCompatActivity() {
 
@@ -23,7 +29,10 @@ abstract class BaseActivityNavigator<VM: ViewModel>(): AppCompatActivity() {
         baseNavHostFragment.navController
     }
 
-    abstract val navigationId: Int
+    val bottomNavigationView: BottomNavigationView by lazy {
+        baseActivityLayoutBinding.bottomNavigationView
+    }
+
     abstract val viewModel: VM
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +40,7 @@ abstract class BaseActivityNavigator<VM: ViewModel>(): AppCompatActivity() {
 
         baseActivityLayoutBinding = BaseActivityLayoutBinding.inflate(layoutInflater)
         baseNavHostFragment = supportFragmentManager.findFragmentById(R.id.base_nav_host_fragment) as NavHostFragment
-        setupActionBarWithNavGraph()
+        bottomNavigationView.visibility = View.GONE
         setContentView(baseActivityLayoutBinding.root)
     }
 
@@ -45,10 +54,24 @@ abstract class BaseActivityNavigator<VM: ViewModel>(): AppCompatActivity() {
         return false
     }
 
-    private fun setupActionBarWithNavGraph() {
-        navController.setGraph(navigationId)
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController,appBarConfiguration)
+    fun setupActionBarWithNavGraph(navigationId: Int) {
+        try {
+            navController.setGraph(navigationId)
+            val appBarConfiguration = AppBarConfiguration(navController.graph)
+            setupActionBarWithNavController(navController,appBarConfiguration)
+        } catch(e: Exception) {
+            e.localizedMessage?.let { Log.e("setupActionBarWithNavGraph", it) }
+        }
+    }
+
+    fun setupBottomBarWithMenu(bottomMenu: Int) {
+        try {
+            bottomNavigationView.menu.clear()
+            bottomNavigationView.inflateMenu(bottomMenu)
+            bottomNavigationView.visibility = View.VISIBLE
+        } catch (e: Exception) {
+            e.localizedMessage?.let { localizedMessage -> Log.e("setupActionBarWithNavGraph", localizedMessage) }
+        }
     }
 
     fun navigateToDestination(resId: Int) {
--- app/src/main/java/com/didebbo/mappify/presentation/view/activity/PreLoginActivity.kt
+++ app/src/main/java/com/didebbo/mappify/presentation/view/activity/PreLoginActivity.kt
@@ -1,7 +1,9 @@
 package com.didebbo.mappify.presentation.view.activity
 
 import android.content.Intent
+import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.didebbo.mappify.presentation.baseclass.activity.BaseActivityNavigator
 import com.didebbo.mappify.presentation.viewmodel.PreLoginViewModel
 import com.github.didebbo.mappify.R
@@ -9,9 +11,13 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PreLoginActivity: BaseActivityNavigator<PreLoginViewModel>() {
-    override val navigationId: Int = R.navigation.pre_login_navigation
+
     override val viewModel: PreLoginViewModel by viewModels()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupActionBarWithNavGraph(R.navigation.pre_login_navigation)
+    }
     fun navigateToPostLogin() {
         navigateToActivity(Intent(this,PostLoginActivity::class.java))
     }
