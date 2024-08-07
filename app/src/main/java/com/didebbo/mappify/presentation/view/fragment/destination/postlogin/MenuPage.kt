c98cbbe66ab59820cdcf55337cc700e7afe76cab
Gianluca Napoletano <gianluca@192.168.1.51>
2024-08-07 11:23:13 +0200

    handle first voices on menu

--- app/src/main/java/com/didebbo/mappify/presentation/view/fragment/destination/postlogin/MenuPage.kt
+++ app/src/main/java/com/didebbo/mappify/presentation/view/fragment/destination/postlogin/MenuPage.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.didebbo.mappify.R
+import com.didebbo.mappify.databinding.MenuPageItemLayoutBinding
 import com.didebbo.mappify.databinding.MenuPageLayoutBinding
 import com.didebbo.mappify.presentation.baseclass.fragment.page.BaseFragmentDestination
 import com.didebbo.mappify.presentation.view.activity.PostLoginActivity
@@ -16,6 +19,21 @@ class MenuPage: BaseFragmentDestination<PostLoginViewModel>(PostLoginViewModel::
 
     private lateinit var binding: MenuPageLayoutBinding
     private lateinit var recyclerView: RecyclerView
+
+
+    private val menuItemsData: List<MenuPageAdapter.ItemViewData> = listOf(
+        MenuPageAdapter.ItemViewData(
+            title = "User Details",
+            action = {}
+        ),
+        MenuPageAdapter.ItemViewData(
+            title = "Add New Marker Point",
+            action = {
+                navController?.navigate(R.id.new_marker_point_navigation_activity)
+            }
+        )
+    )
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,8 +48,42 @@ class MenuPage: BaseFragmentDestination<PostLoginViewModel>(PostLoginViewModel::
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        recyclerView.layoutManager = LinearLayoutManager(context).apply { orientation = LinearLayoutManager.VERTICAL }
+        recyclerView.adapter = MenuPageAdapter(menuItemsData)
+
+
         binding.logOutItemMenu.setOnClickListener{
             postLoginActivity?.navigateToPreLogin()
         }
+    }
+}
+
+class MenuPageAdapter(private val data: List<ItemViewData>): RecyclerView.Adapter<MenuPageAdapter.ItemView>() {
+
+    class ItemViewData(
+        val title: String,
+        val action: ()->Unit
+    )
+    class ItemView(val binding: MenuPageItemLayoutBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemView {
+        val binding = MenuPageItemLayoutBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ItemView(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: ItemView, position: Int) {
+        val item = getItemViewData(position)
+        holder.binding.menuItem.text = item.title
+        holder.binding.menuItem.setOnClickListener {
+            item.action()
+        }
+    }
+
+    private fun getItemViewData(position: Int): ItemViewData {
+        return data[position]
     }
 }
